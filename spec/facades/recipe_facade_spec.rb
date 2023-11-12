1e4c9c1c2ef04fc1e599390124c4b802d65e8eaa
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-11-12 15:10:49 -0700

    test: expand on testing for the recipe facade and service

--- spec/facades/recipe_facade_spec.rb
+++ spec/facades/recipe_facade_spec.rb
@@ -1,5 +1,18 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe RecipeFacade do
-#   describe ""
-# end
+RSpec.describe RecipeFacade do
+  describe "#recipe_search" do
+    it "can return recipes related to the country search" do
+      VCR.use_cassette("recipe_service_search_thailand") do
+        recipes_returned = RecipeFacade.new.recipe_search("thailand")
+
+        recipe = recipes_returned[0]
+
+        expect(recipe.id).to eq(nil)
+        expect(recipe.title).to be_a String
+        expect(recipe.url).to be_a String
+        expect(recipe.image).to be_a String
+      end
+    end
+  end
+end
--- spec/services/recipe_service_spec.rb
+++ spec/services/recipe_service_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Recipe Service", type: :service do
+RSpec.describe RecipeService, type: :service do
   describe "search by country country" do
     it "can search by country" do
       VCR.use_cassette("recipe_service_search_thailand") do 
@@ -8,6 +8,7 @@ RSpec.describe "Recipe Service", type: :service do
         
         expect(recipes_returned).to be_a Hash 
         expect(recipes_returned[:hits]).to be_an Array
+        expect(recipes_returned[:hits][0][:recipe]).to be_a Hash
       end
     end
   end
