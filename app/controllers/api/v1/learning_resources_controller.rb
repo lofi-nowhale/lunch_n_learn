1c992fac6c1b197b6e92e6ac9179e247d0d0e8e1
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-11-14 16:07:50 -0700

    FINISH USER STORY 2

--- app/controllers/api/v1/learning_resources_controller.rb
+++ app/controllers/api/v1/learning_resources_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::LearningResourcesController < ApplicationController
   def index 
-    @resource = LearningResourceFacade.new.learning_resource(country)
+    @resource = LearningResourceFacade.new.learning_resource(params[:country])
     
-    render json: LearningResourcesSerializer.new(@resource)
+    render json: LearningResourceSerializer.new(@resource)
   end
 end
