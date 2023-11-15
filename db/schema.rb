105c856c3b9e249d9d9b687d9333657472047cf5
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-11-15 01:48:56 -0700

    create + test: test for the favorites table, create the favorites table, establish relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,32 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_11_204731) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_15_080650) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "countries", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "recipes", force: :cascade do |t|
-    t.string "title"
-    t.string "url"
+  create_table "favorites", force: :cascade do |t|
     t.string "country"
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "user_recipes", force: :cascade do |t|
+    t.string "recipe_link"
+    t.string "recipe_title"
     t.bigint "user_id", null: false
-    t.bigint "recipe_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["recipe_id"], name: "index_user_recipes_on_recipe_id"
-    t.index ["user_id"], name: "index_user_recipes_on_user_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -47,6 +33,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_11_204731) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "user_recipes", "recipes"
-  add_foreign_key "user_recipes", "users"
+  add_foreign_key "favorites", "users"
 end
